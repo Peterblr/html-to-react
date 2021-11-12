11f2a57619b35e093abefff503ac3220ecd2be45
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-12 09:35:39 +0300

    edited the code in section 3

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -10,19 +10,26 @@ const Section3 = () => {
                             <div className="u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
                                 <div className="u-container-layout">
                                     <img src="images/161ab6cb425b7ec43838e8487c441a17.png" alt=""
-                                         className="u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xl u-image u-image-default u-opacity u-opacity-10 u-image-1"
-                                         data-image-width="1000" data-image-height="500">
-                                        <h2 className="u-custom-font u-font-roboto-slab u-text u-text-1">Apple W1
-                                            Chip</h2>
-                                        <h6 className="u-text u-text-custom-color-2 u-text-2">custom comfort</h6>
-                                        <p className="u-text u-text-3">Beats Solo3 Wireless stays true to its predecessor
-                                            with bold styling and a streamlined design. The on-ear, cushioned ear cups are
-                                            adjustable so you can customize your fit for all-day listening comfort. The
-                                            headphone’s fast-flowing
-                                            curves and pivoting ear cups complete this natural fit, designed for optimal
-                                            comfort and sound delivery.</p>
+                                         className="u-expanded-width-lg u-expanded-width-md u-expanded-width-sm
+                                          u-expanded-width-xl u-image u-image-default u-opacity u-opacity-10 u-image-1"
+                                         data-image-width="1000" data-image-height="500"/>
+                                        <h2 className="u-custom-font u-font-roboto-slab u-text u-text-1">
+                                            Apple W1 Chip
+                                        </h2>
+                                        <h6 className="u-text u-text-custom-color-2 u-text-2">
+                                            custom comfort
+                                        </h6>
+                                        <p className="u-text u-text-3">
+                                            Beats Solo3 Wireless stays true to its predecessor
+                                            with bold styling and a streamlined design. The on-ear, cushioned ear cups
+                                            are adjustable so you can customize your fit for all-day listening comfort.
+                                            The headphone’s fast-flowing curves and pivoting ear cups complete this
+                                            natural fit, designed for optimal comfort and sound delivery.
+                                        </p>
                                         <a href="https://nicepage.cc"
-                                           className="u-btn u-btn-round u-button-style u-radius-24 u-btn-1">read more</a>
+                                           className="u-btn u-btn-round u-button-style u-radius-24 u-btn-1">
+                                            read more
+                                        </a>
                                 </div>
                             </div>
                             <div className="u-container-style u-image u-layout-cell u-right-cell u-size-30 u-image-2">
@@ -33,8 +40,7 @@ const Section3 = () => {
                 </div>
             </div>
         </section>
-
-);
+    );
 };
 
 export default Section3;
