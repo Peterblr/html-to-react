64841637a5405423e1875e07beb1ce1009389ec2
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-12 09:47:15 +0300

    edited the code in section 9

--- src/components/Section9.jsx
+++ src/components/Section9.jsx
@@ -8,17 +8,23 @@ const Section9 = () => {
                     <div className="u-layout">
                         <div className="u-layout-row">
                             <div
-                                className="u-container-style u-layout-cell u-left-cell u-size-20 u-size-20-md u-layout-cell-1">
+                                className="u-container-style u-layout-cell u-left-cell u-size-20
+                                u-size-20-md u-layout-cell-1">
                                 <div className="u-container-layout">
                                     <img className="u-expanded-width-xs u-image u-image-1"
-                                         src="images/4670ed0bb0bdcb49420a265f0305d6b0.png">
+                                         src="images/4670ed0bb0bdcb49420a265f0305d6b0.png"/>
                                 </div>
                             </div>
                             <div
-                                className="u-container-style u-layout-cell u-size-20-lg u-size-20-xl u-size-21-md u-size-21-sm u-size-21-xs u-layout-cell-2">
+                                className="u-container-style u-layout-cell u-size-20-lg u-size-20-xl u-size-21-md
+                                 u-size-21-sm u-size-21-xs u-layout-cell-2">
                                 <div className="u-container-layout">
-                                    <h2 className="u-custom-font u-font-roboto-slab u-text u-text-1">Tech Specs</h2>
-                                    <h5 className="u-text u-text-custom-color-2 u-text-2">Manufacturer Information</h5>
+                                    <h2 className="u-custom-font u-font-roboto-slab u-text u-text-1">
+                                        Tech Specs
+                                    </h2>
+                                    <h5 className="u-text u-text-custom-color-2 u-text-2">
+                                        Manufacturer Information
+                                    </h5>
                                     <ul className="u-spacing-16 u-text u-text-3">
                                         <li>Form Factor: On Ear</li>
                                         <li>Connections: Bluetooth, Wireless</li>
@@ -26,15 +32,20 @@ const Section9 = () => {
                                         <li>Height: 7.8 in / 19.8 cm</li>
                                         <li>Weight: 7.58 oz / 215 g</li>
                                         <li>Stereo Bluetooth</li>
-                                        <li>Passive Playback via RemoteTalk cable<br>
+                                        <li>
+                                            Passive Playback via RemoteTalk cable
+                                            <br/>
                                         </li>
                                     </ul>
                                 </div>
                             </div>
                             <div
-                                className="u-container-style u-layout-cell u-right-cell u-size-19-md u-size-19-sm u-size-19-xs u-size-20-lg u-size-20-xl u-layout-cell-3">
+                                className="u-container-style u-layout-cell u-right-cell u-size-19-md u-size-19-sm
+                                 u-size-19-xs u-size-20-lg u-size-20-xl u-layout-cell-3">
                                 <div className="u-container-layout u-valign-middle-sm u-container-layout-3">
-                                    <h6 className="u-text u-text-custom-color-2 u-text-4">Other Features</h6>
+                                    <h6 className="u-text u-text-custom-color-2 u-text-4">
+                                        Other Features
+                                    </h6>
                                     <ul className="u-spacing-16 u-text u-text-5">
                                         <li>Bluetooth</li>
                                         <li>On-Board Call and Music Controls</li>
@@ -51,8 +62,7 @@ const Section9 = () => {
                 </div>
             </div>
         </section>
-
-);
+    );
 };
 
 export default Section9;
