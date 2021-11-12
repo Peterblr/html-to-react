d7c0417ff77643fea727b3b857b9ccc76166c34f
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-12 09:33:07 +0300

    edited the code in section 2

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -24,19 +24,23 @@ const Section1 = () => {
                 <img src="images/799e71d993a42276772cc9d65de5c6bc.png" alt=""
                      className="u-image u-image-default u-image-1"/>
                     <div
-                        className="u-align-right u-container-style u-expanded-width-md u-expanded-width-xs u-group u-group-2">
+                        className="u-align-right u-container-style u-expanded-width-md u-expanded-width-xs
+                         u-group u-group-2">
                         <div
-                            className="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-2">
-                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-palette-1-base u-title u-text-4">
+                            className="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm
+                             u-valign-middle-xs u-container-layout-2">
+                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-palette-1-base
+                             u-title u-text-4">
                                 Beats Solo3&nbsp;
                             </h1>
-                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-body-alt-color u-title u-text-5">
+                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-body-alt-color
+                             u-title u-text-5">
                                 Wireless
                             </h1>
                             <p className="u-text u-text-body-alt-color u-text-6">
                                 With up to 40 hours of battery life,
-                                Beats Solo3 Wireless is your perfect everyday headphone. With Fast Fuel, a 5-minute charge
-                                gives you 3 hours of playback.
+                                Beats Solo3 Wireless is your perfect everyday headphone.
+                                With Fast Fuel, a 5-minute charge gives you 3 hours of playback.
                             </p>
                             <a href="https://nicepage.com/k/youtube-website-templates"
                                className="u-btn u-btn-round u-button-style u-radius-24 u-btn-2">
