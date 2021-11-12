df423d4e1d5417fb9e1c0553071d0321bf6b7c97
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-12 10:28:14 +0300

    edited image names in Home.scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './scss/App.scss';
+import Section1 from "./components/Section1";
 
 function App() {
   return (
       <body className="u-body u-overlap u-overlap-contrast">
+      <Section1/>
       {/*<style className="u-overlap-style">*/}
       {/*    .u-overlap: not(.u-sticky-scroll)*/}
       {/*    .u-header {*/}
