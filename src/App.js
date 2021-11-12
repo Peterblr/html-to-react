0a4465ef39d192b648dffe628e9901b1b184977d
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-12 09:29:59 +0300

    edited the code in section 1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './scss/App.scss';
 function App() {
   return (
       <body className="u-body u-overlap u-overlap-contrast">
-      <style className="u-overlap-style">.u-overlap:not(.u-sticky-scroll) .u-header {
-          background - color: #f15048 !important
-      }</style>
+      {/*<style className="u-overlap-style">*/}
+      {/*    .u-overlap: not(.u-sticky-scroll)*/}
+      {/*    .u-header {*/}
+      {/*      background-color: #f15048 !important*/}
+      {/*       }*/}
+      {/*</style>*/}
 
 
       </body>
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -6,36 +6,47 @@ const Section1 = () => {
             <div className="u-clearfix u-sheet u-sheet-1">
                 <div className="u-align-right u-container-style u-expanded-width-xs u-group u-group-1">
                     <div className="u-container-layout u-valign-middle-lg u-valign-middle-md u-container-layout-1">
-                        <h1 className="u-custom-font u-font-roboto-slab u-text u-text-palette-1-base u-title u-text-1">Training
-                            Project</h1>
-                        <h1 className="u-custom-font u-font-roboto-slab u-text u-text-body-alt-color u-title u-text-2">Piotr
-                            Sviryd</h1>
-                        <p className="u-text u-text-body-alt-color u-text-3">My first project where i remade HTML website
-                            to React.</p>
+                        <h1 className="u-custom-font u-font-roboto-slab u-text u-text-palette-1-base u-title u-text-1">
+                            Training Project
+                        </h1>
+                        <h1 className="u-custom-font u-font-roboto-slab u-text u-text-body-alt-color u-title u-text-2">
+                            Piotr Sviryd
+                        </h1>
+                        <p className="u-text u-text-body-alt-color u-text-3">
+                            My first project where i remade HTML website to React.
+                        </p>
                         <a href="https://nicepage.com/k/youtube-website-templates"
-                           className="u-btn u-btn-round u-button-style u-radius-24 u-btn-1"> wiew on github</a>
+                           className="u-btn u-btn-round u-button-style u-radius-24 u-btn-1">
+                            wiew on github
+                        </a>
                     </div>
                 </div>
                 <img src="images/799e71d993a42276772cc9d65de5c6bc.png" alt=""
-                     className="u-image u-image-default u-image-1">
+                     className="u-image u-image-default u-image-1"/>
                     <div
                         className="u-align-right u-container-style u-expanded-width-md u-expanded-width-xs u-group u-group-2">
                         <div
                             className="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-2">
-                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-palette-1-base u-title u-text-4">Beats
-                                Solo3&nbsp;</h1>
-                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-body-alt-color u-title u-text-5">Wireless</h1>
-                            <p className="u-text u-text-body-alt-color u-text-6">With up to 40 hours of battery life,
+                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-palette-1-base u-title u-text-4">
+                                Beats Solo3&nbsp;
+                            </h1>
+                            <h1 className="u-custom-font u-font-roboto-slab u-text u-text-body-alt-color u-title u-text-5">
+                                Wireless
+                            </h1>
+                            <p className="u-text u-text-body-alt-color u-text-6">
+                                With up to 40 hours of battery life,
                                 Beats Solo3 Wireless is your perfect everyday headphone. With Fast Fuel, a 5-minute charge
-                                gives you 3 hours of playback.</p>
+                                gives you 3 hours of playback.
+                            </p>
                             <a href="https://nicepage.com/k/youtube-website-templates"
-                               className="u-btn u-btn-round u-button-style u-radius-24 u-btn-2">$299.95 Buy</a>
+                               className="u-btn u-btn-round u-button-style u-radius-24 u-btn-2">
+                                $299.95 Buy
+                            </a>
                         </div>
                     </div>
             </div>
         </section>
-
-);
+    );
 };
 
 export default Section1;
