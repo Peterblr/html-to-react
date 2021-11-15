82534f1b02a4a64ee10b601c8346350965614f8c
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-15 09:36:17 +0300

    finished section 8

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Section4 from "./components/Section4";
 import Section5 from "./components/Section5";
 import Section6 from "./components/Section6";
 import Section7 from "./components/Section7";
+import Section8 from "./components/Section8";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       {/*<Section5/>*/}
       <Section6/>
       <Section7/>
+      <Section8/>
       {/*<style className="u-overlap-style">*/}
       {/*    .u-overlap: not(.u-sticky-scroll)*/}
       {/*    .u-header {*/}
