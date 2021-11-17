62820bae529dc9a2af792a66349dc2f45b5b1f24
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-17 15:08:35 +0300

    add styles without adaptation page

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import s from "../scss/Section4.module.scss"
 
 const Section4 = () => {
     return (
-        <section className="u-clearfix u-section-4" id="sec-32e2">
-            <div className="u-align-left u-expanded u-left-0 u-video">
-                <div className="embed-responsive">
-                    <iframe className="embed-responsive-item"
-                            title="This is a unique title"
-                            src="https://www.youtube.com/embed/MlCqfK09u7A?loop=0&amp;showinfo=0&amp;
+        <section>
+            <div className={s.video}>
+                <iframe width="100%"
+                        height="100%"
+                    title="This is a unique title"
+                    src="https://www.youtube.com/embed/MlCqfK09u7A?loop=0&amp;showinfo=0&amp;
                             controls=0&amp;start=0"
-                            frameBorder="0" allowFullScreen/>
-                </div>
+                    frameBorder="0" allowFullScreen/>
             </div>
         </section>
     );
